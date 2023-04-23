9a53578c49e4ef69a1cfce4fa0c0200f25bd67d1
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-04-23 20:46:35 -0300

    update criminal record viewmodel with UT 😊👌

--- src/modules/create_criminalrecord/app/create_criminalrecord_viewmodel.py
+++ src/modules/create_criminalrecord/app/create_criminalrecord_viewmodel.py
@@ -1,4 +1,3 @@
-
 from src.shared.domain.entities.crime import Crime
 from src.shared.domain.entities.criminal import Criminal
 from src.shared.domain.entities.criminalrecord import CriminalRecord
@@ -6,20 +5,21 @@ from src.shared.domain.enums.crime_enum import CRIME
 from src.shared.domain.enums.danger_score_enum import DANGER_SCORE
 from src.shared.domain.enums.gender_enum import GENDER
 
+
 class CriminalViewmodel:
     id: int
     name: str
     description: str
     gender: GENDER
     region: str
-    
+
     def __init__(self, criminal: Criminal):
         self.id = criminal.id
         self.name = criminal.name
         self.description = criminal.description
         self.gender = criminal.gender
         self.region = criminal.region
-        
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -29,14 +29,15 @@ class CriminalViewmodel:
             "region": self.region,
         }
 
+
 class CrimeViewmodel:
     id: int
     criminal: Criminal
     crime: CRIME
     region: str
-    date: str # DD-MM-YYYY
+    date: str  # DD-MM-YYYY
     num_victims: int
-    
+
     def __init__(self, crime: Crime):
         self.id = crime.id
         self.criminal = CriminalViewmodel(crime.criminal)
@@ -44,7 +45,7 @@ class CrimeViewmodel:
         self.region = crime.region
         self.date = crime.date
         self.num_victims = crime.num_victims
-        
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -55,20 +56,21 @@ class CrimeViewmodel:
             "num_victims": self.num_victims,
         }
 
+
 class CriminalRecordViewmodel:
     id: int
     criminal: Criminal
     crimes: list[Crime]
     arrested: bool
     score: DANGER_SCORE
-    
+
     def __init__(self, criminalrecord: CriminalRecord):
         self.id = criminalrecord.id
         self.criminal = CriminalViewmodel(criminalrecord.criminal)
         self.crimes = [CrimeViewmodel(crime) for crime in criminalrecord.crimes]
         self.arrested = criminalrecord.arrested
         self.score = criminalrecord.score
-        
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -81,12 +83,12 @@ class CriminalRecordViewmodel:
 
 class CreateCriminalRecordViewmodel:
     criminalrecord: CriminalRecordViewmodel
-    
+
     def __init__(self, criminalrecord: CriminalRecord):
         self.criminalrecord = CriminalRecordViewmodel(criminalrecord)
-        
+
     def to_dict(self):
         return {
-            'crime': self.criminalrecord.to_dict(),
-            'massage': "the criminal record was created",
+            "crime": self.criminalrecord.to_dict(),
+            "message": "the criminal record was created",
         }
--- tests/modules/create_criminalrecord/app/test_create_criminalrecord_viewmodel.py
+++ tests/modules/create_criminalrecord/app/test_create_criminalrecord_viewmodel.py
@@ -1,45 +1,49 @@
-from src.modules.create_criminalrecord.app.create_criminalrecord_viewmodel import CreateCriminalRecordViewmodel
-from src.shared.infra.repositories.criminalrecord_repository_mock import CriminalRecordRepositoryMock
+from src.modules.create_criminalrecord.app.create_criminalrecord_viewmodel import (
+    CreateCriminalRecordViewmodel,
+)
+from src.shared.infra.repositories.criminalrecord_repository_mock import (
+    CriminalRecordRepositoryMock,
+)
 
 
 class Test_CreateCriminalRecordViewmodel:
     def test_create_criminalrecord_viewmodel(self):
         repo = CriminalRecordRepositoryMock()
-        
+
         criminalrecord = repo.criminalrecords[0]
-        
-        criminalrecord_viewmodel = CreateCriminalRecordViewmodel(criminalrecord).to_dict()
-        
+
+        criminalrecord_viewmodel = CreateCriminalRecordViewmodel(
+            criminalrecord
+        ).to_dict()
+
         assert criminalrecord_viewmodel == {
-            "crime":{
-                "id":1,
-                "criminal":{
-                    "id":1,
-                    "name":"Furlan",
-                    "description":"Furlan is a Automato",
-                    "gender":"MALE",
-                    "region":"Mauá"
+            "crime": {
+                "id": 1,
+                "criminal": {
+                    "id": 1,
+                    "name": "Furlan",
+                    "description": "Furlan is a Automato",
+                    "gender": "MALE",
+                    "region": "Mauá",
                 },
-                "crimes":[
+                "crimes": [
                     {
-                        "id":1,
-                        "criminal":{
-                        "id":1,
-                        "name":"Furlan",
-                        "description":"Furlan is a Automato",
-                        "gender":"MALE",
-                        "region":"Mauá"
+                        "id": 1,
+                        "criminal": {
+                            "id": 1,
+                            "name": "Furlan",
+                            "description": "Furlan is a Automato",
+                            "gender": "MALE",
+                            "region": "Mauá",
                         },
-                        "crime":"MURDER",
-                        "region":"Mauá",
-                        "date":"20-01-2021",
-                        "num_victims":1
+                        "crime": "MURDER",
+                        "region": "Mauá",
+                        "date": "20-01-2021",
+                        "num_victims": 1,
                     }
                 ],
-                "arrested":False,
-                "score":"LIGHT"
+                "arrested": False,
+                "score": "LIGHT",
             },
-            "massage":"the criminal record was created"
-            }
-            
-            
+            "message": "the criminal record was created",
+        }
