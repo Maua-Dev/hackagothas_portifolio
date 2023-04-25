07f6af244d72d3f353f27db24301d1bfdd632a59
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-04-25 18:07:07 -0300

    DELETE CRIMINAL RECORD PRESENTER WITH UT AND VIEWMODEL FINISHED 🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣

--- tests/modules/delete_criminalrecord/app/test_delete_criminalrecord_controller.py
+++ tests/modules/delete_criminalrecord/app/test_delete_criminalrecord_controller.py
@@ -26,6 +26,16 @@ class Test_DeleteCriminalRecordController:
 
         assert response.status_code == 200
         assert response.body["message"] == "the criminal record was deleted"
+        assert response.body["criminalrecord"]["id_criminalrecord"] == 1
+        assert response.body["criminalrecord"]["criminal"]["name"] == "Furlan"
+        assert (
+            response.body["criminalrecord"]["criminal"]["description"]
+            == "Furlan is a Automato"
+        )
+        assert response.body["criminalrecord"]["criminal"]["gender"] == "MALE"
+        assert response.body["criminalrecord"]["criminal"]["region_criminal"] == "Mauá"
+        assert response.body["criminalrecord"]["arrested"] == False
+        assert response.body["criminalrecord"]["score"] == "ONESTAR"
 
     def test_delete_criminalrecord_controller_invalid_id(self):
         repo = CriminalRecordRepositoryMock()
